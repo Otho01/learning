f1fa07c9f3aabdd6e7650f26029c1eb2b2ff8515
Luis <bhrams1@gmail.com>
2021-03-01 11:54:50 -0500

    Added reusable bottom

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -3,20 +3,17 @@ import React from "react"
 import './App.css';
 
 class Button extends React.Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
+    const {children} = this.props
     return (
-      <button></button>
+      <button>
+        {children}
+      </button>
     )
   }
 }
 
 class SingInForm extends React.Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
     return (
       <form className="signInForm">
@@ -28,15 +25,18 @@ class SingInForm extends React.Component {
           <br/>
           <p>Password</p>
           <input type="password" className="inputs"/>
-          <Button className="buttons" />
+          <Button className="buttons" >
+            Sign in
+          </Button>
+          <Button className="buttons" >
+            Sign up
+          </Button>
           <a className="anchors" href="#">Forgot password?</a>
         </fieldset>
       </form>
     )
   }
 }
-
-
 
 function App() {
   return (
